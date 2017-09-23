3fad756f183575032484f1a45ecde0667a26a627
aprosikhin <aprosikhin@freewheel.tv>
2017-09-23 14:44:21 -0400

    DB security group created.

--- network_deployment/launch_network.sh
+++ network_deployment/launch_network.sh
@@ -9,16 +9,20 @@ echo $VpcID
 echo 'Loading Subnet ID:'
 SubnetID=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VpcID --query Subnets[0].SubnetId --output text)
 echo $SubnetID
+echo 'Loading Subnet Two ID:'
+SubnetTwoID=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VpcID --query Subnets[1].SubnetId --output text)
+echo $SubnetTwoID
 
 aws cloudformation create-stack --stack-name ET-Network \
 --capabilities CAPABILITY_IAM \
 --template-body file://network.json \
 --parameters ParameterKey=VPC,ParameterValue=$VpcID \
 ParameterKey=Subnet,ParameterValue=$SubnetID \
+ParameterKey=SubnetTwo,ParameterValue=$SubnetTwoID \
 ParameterKey=InstanceType,ParameterValue=t2.micro
 
 echo 'Stack creation in progress'
-aws cloudformation wait stack-create-complete --stack-name Tofu
+aws cloudformation wait stack-create-complete --stack-name ET-Network
 echo 'Network Stack is created'
 
 set +e
